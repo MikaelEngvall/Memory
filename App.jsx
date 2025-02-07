8680ed1e31d0fc6bcd3aa6b72d004941be9c3a32
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-02-07 03:49:18 +0100

    Initial commit

--- App.jsx
+++ App.jsx
@@ -7,7 +7,11 @@ import GameOver from '/components/GameOver'
 import GameStatus from './components/GameStatus'
 import ErrorCard from '/components/ErrorCard'
 
-const socket = io('http://localhost:3001')
+const socket = io(
+  import.meta.env.PROD 
+    ? 'din-produktions-server-url' 
+    : 'http://localhost:3001'
+)
 
 export default function App() {
     const initialFormData = {
